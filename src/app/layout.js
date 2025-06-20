8cf69a1a564b127eeb36b240beb9ef8a97879b8d
bhaskarpathriya <bhaskarpathriya829@gmail.com>
2025-06-20 15:22:36 +0530

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import MainLayout from "@/components/MainLayout";
 import { ModalProvider } from "@/context/ModalContext";
 import { Toaster } from "react-hot-toast";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Digital Solutions",
@@ -22,9 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="no-scrollbar">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased no-scrollbar`}
-      >
+      <body className={`antialiased no-scrollbar`}>
         <Toaster />
         <ModalProvider>
           <MainLayout>{children}</MainLayout>
